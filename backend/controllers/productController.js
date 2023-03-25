d1355a15be119cf4b65a7ca97f3274879a2aef7c
SivaGitRepo <siva.smoorthy@gmail.com>
2023-03-25 22:32:12 +0530

    Ver 1 of changes until 25-Mar-2023

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-exports.getProducts = () => (req, res, next) => {
+exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success:true,
         message: "This route will show all the products in database"
