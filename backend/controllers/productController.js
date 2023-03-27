c70177966fc9aac028e0059ecc7ff1ad7e6316f1
SivaGitRepo <siva.smoorthy@gmail.com>
2023-03-27 08:31:40 +0530

    Ver 1 of changes until 27-Mar-2023

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,61 @@
 const Product = require('../models/productModel');
-exports.getProducts = (req, res, next) => {
+
+//Get products - api/v1/products
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.find();
     res.status(200).json({
         success:true,
-        message: "This route will show all the products in database"
+        count: products.length,
+        products: products
     })
 }
 
+//Get single product - api/v1/product
+exports.getProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    res.status(201).json({
+        success:true,
+        product: product
+    })
+}
+
+//Create product - api/v1/product/new
 exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     console.log(res);
     res.status(201).json({
         success:true,
+        product: product
+    })
+}
+
+//Update product - api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    console.log(req.body);
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    
+    res.status(200).json({
+        success: true,
         product: product
     })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getProducts, newProduct } = require('../controllers/productController');
+const { getProducts, newProduct, getProduct, updateProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
-
 router.route('/product/new').post(newProduct);
+router.route('/product/:id').get(getProduct)
+                            .put(updateProduct);
 
 module.exports = router;
