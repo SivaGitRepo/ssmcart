4986515597fd16fad818291e5449892f4ef54766
SivaGitRepo <siva.smoorthy@gmail.com>
2023-03-30 22:42:34 +0530

    Ver2 of changes until 30-Mar-2023

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,12 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
-const catchAsyncError = require('../middlewares/catchAsyncError')
+const catchAsyncError = require('../middlewares/catchAsyncError');
+const APIFeatures = require('../utils/apiFeatures');
 
 //Get products - api/v1/products
 exports.getProducts = async (req, res, next) => {
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter();
+    const products = await apiFeatures.query;
     res.status(200).json({
         success:true,
         count: products.length,
