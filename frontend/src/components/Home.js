01ac976f7974b2c01422c98e95465c444a43e721
SivaGitRepo <siva.smoorthy@gmail.com>
2023-07-16 21:55:15 +0530

    Ver 1 of changes until 16-Jul-2023

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,63 +1,67 @@
 import { Fragment, useEffect } from "react";
-import MetaData from './layouts/MetaData';
-import { useDispatch, useSelector } from 'react-redux';
+import MetaData from "./layouts/MetaData";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { getProducts } from "../actions/productsActions";
 import Loader from "./layouts/Loader";
 import Product from "./product/Product";
 import { toast } from "react-toastify";
-import Pagination from 'react-js-pagination';
+import Pagination from "react-js-pagination";
 
-export default function Home () {
-    
-    const dispatch = useDispatch();
-    const { products, loading, error, productsCount, resultsPerPage } = useSelector((state) => state.productsState);
-    const [currentPage, setCurrentPage] = useState(1);
+export default function Home() {
+  const dispatch = useDispatch();
+  const { products, loading, error, productsCount, resultsPerPage } =
+    useSelector((state) => state.productsState);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const setCurrentPageNo = (pageNo) => {
-        setCurrentPage(pageNo)
+  const setCurrentPageNo = (pageNo) => {
+    setCurrentPage(pageNo);
+  };
+
+  useEffect(() => {
+    if (error) {
+      return toast.error("hello ssm cart user", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     }
+    console.log("currrrrrent: ", currentPage);
+    dispatch(getProducts(currentPage, null));
+    //dispatch(getProducts);
+  }, [error, dispatch, currentPage]);
 
-    useEffect(() => {
-        if (error) {
-            return toast.error ('hello ssm cart user',{
-                position: toast.POSITION.BOTTOM_RIGHT
-        })
-    }
-        console.log('currrrrrent: ', currentPage)
-        dispatch(getProducts(currentPage, null))
-        //dispatch(getProducts);
-    }, [error, dispatch, currentPage])
-    
-    return (
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? <Loader/> :
-                <Fragment>
-                    <MetaData title={'Buy best products'}/>
-                    <h1 id="products_heading">Latest Products</h1>
-                    <section id="products" className="container mt-5">
-                    <div className="row">
-                        { products && products.map(product => (
-                            <Product key={product._id} product={product}/>
-                        )) }
-                    </div>
-                    </section>
-                    {productsCount > 0 && productsCount > resultsPerPage ?
-                        <div className="d-flex justify-content-center mt-5">
-                            <Pagination
-                                activePage={currentPage}
-                                onChange={setCurrentPageNo}
-                                totalItemsCount={productsCount}
-                                itemsCountPerPage={resultsPerPage}
-                                nextPageText={'Next'}
-                                firstPageText={'First'}
-                                lastPageText={'Last'}
-                                itemClass={'page-item'}
-                                linkClass={'page-link'}
-                            />
-                    </div> : null }
-                </Fragment>
-            }
+          <MetaData title={"Buy best products"} />
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product col={3} key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+          {productsCount > 0 && productsCount > resultsPerPage ? (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                onChange={setCurrentPageNo}
+                totalItemsCount={productsCount}
+                itemsCountPerPage={resultsPerPage}
+                nextPageText={"Next"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass={"page-item"}
+                linkClass={"page-link"}
+              />
+            </div>
+          ) : null}
         </Fragment>
-    )
+      )}
+    </Fragment>
+  );
 }
--- frontend/src/components/product/ProductSearch.js
+++ frontend/src/components/product/ProductSearch.js
@@ -1,63 +1,74 @@
 import { Fragment, useEffect } from "react";
-import MetaData from '../layouts/MetaData';
-import { useDispatch, useSelector } from 'react-redux';
+import MetaData from "../layouts/MetaData";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { getProducts } from "../../actions/productsActions";
 import Loader from "../layouts/Loader";
 import Product from "../product/Product";
 import { toast } from "react-toastify";
-import Pagination from 'react-js-pagination';
+import Pagination from "react-js-pagination";
 import { useParams } from "react-router-dom";
 
-export default function ProductSearch () {
-    
-    const dispatch = useDispatch();
-    const { products, loading, error, productsCount, resultsPerPage } = useSelector((state) => state.productsState);
-    const [currentPage, setCurrentPage] = useState(1);
-    const { keyword } = useParams();
+export default function ProductSearch() {
+  const dispatch = useDispatch();
+  const { products, loading, error, productsCount, resultsPerPage } =
+    useSelector((state) => state.productsState);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { keyword } = useParams();
 
-    const setCurrentPageNo = (pageNo) => {
-        setCurrentPage(pageNo)
-    }
+  const setCurrentPageNo = (pageNo) => {
+    setCurrentPage(pageNo);
+  };
 
-    useEffect(() => {
-        if (error) {
-            return toast.error ('hello ssm cart user',{
-                position: toast.POSITION.BOTTOM_RIGHT
-        })
+  useEffect(() => {
+    if (error) {
+      return toast.error("hello ssm cart user", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     }
-        dispatch(getProducts(currentPage, keyword))
-    }, [error, dispatch, currentPage, keyword])
-    
-    return (
+    dispatch(getProducts(currentPage, keyword));
+  }, [error, dispatch, currentPage, keyword]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? <Loader/> :
-                <Fragment>
-                    <MetaData title={'Buy best products'}/>
-                    <h1 id="products_heading">Search Products</h1>
-                    <section id="products" className="container mt-5">
-                    <div className="row">
-                        { products && products.map(product => (
-                            <Product key={product._id} product={product}/>
-                        )) }
-                    </div>
-                    </section>
-                    {productsCount > 0 && productsCount > resultsPerPage ?
-                        <div className="d-flex justify-content-center mt-5">
-                            <Pagination
-                                activePage={currentPage}
-                                onChange={setCurrentPageNo}
-                                totalItemsCount={productsCount}
-                                itemsCountPerPage={resultsPerPage}
-                                nextPageText={'Next'}
-                                firstPageText={'First'}
-                                lastPageText={'Last'}
-                                itemClass={'page-item'}
-                                linkClass={'page-link'}
-                            />
-                    </div> : null }
-                </Fragment>
-            }
+          <MetaData title={"Buy best products"} />
+          <h1 id="products_heading">Search Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              <div className="col-6 col-md-3 mb-5 mt-5">
+                <div className="px-5"></div>
+              </div>
+              <div className="col-6 col-md-9">
+                <div className="row">
+                  {products &&
+                    products.map((product) => (
+                      <Product col={4} key={product._id} product={product} />
+                    ))}
+                </div>
+              </div>
+            </div>
+          </section>
+          {productsCount > 0 && productsCount > resultsPerPage ? (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                onChange={setCurrentPageNo}
+                totalItemsCount={productsCount}
+                itemsCountPerPage={resultsPerPage}
+                nextPageText={"Next"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass={"page-item"}
+                linkClass={"page-link"}
+              />
+            </div>
+          ) : null}
         </Fragment>
-    )
+      )}
+    </Fragment>
+  );
 }
